dfcdbb650b9f1702dcb8b7453405feabde88ef1d
Daniel S. Filho <danielbrumdasilva@gmail.com>
2023-09-07 18:13:09 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,30 @@
+import exceptions.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = sc.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }catch (ParametrosInvalidosException exception) {
+            exception.printStackTrace();
+        }
+        sc.close();
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
